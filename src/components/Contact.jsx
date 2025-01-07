767f53a31ae38c48541a4a4fd579d9cba13e87a3
Seckin Ozturk <franseck102@gmail.com>
2025-01-06 20:13:06 -0500

    contact page style

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -45,42 +45,37 @@ const Contact = () => {
             or large projects. However, if you have any other requests or
             questions, don't hesitate to contact me using below form either.
           </p>
+</div>
+
           <div className="contact-form">
             <form ref={form} onSubmit={sendEmail}>
-              <ul>
-                <li className="half">
+                      
                   <input placeholder="Name" type="text" name="name" required />
-                </li>
-                <li className="half">
-                  <input
+                     <input
                     placeholder="Email"
                     type="email"
                     name="email"
                     required
                   />
-                </li>
-                <li>
+               
                   <input
                     placeholder="Subject"
                     type="text"
                     name="subject"
                     required
                   />
-                </li>
-                <li>
+        
                   <textarea
                     placeholder="Message"
                     name="message"
                     required
                   ></textarea>
-                </li>
-                <li>
+           <div></div>
                   <input type="submit" className="flat-button" value="SEND" />
-                </li>
-              </ul>
+          
             </form>
           </div>
-        </div>
+    
          
                    
 
