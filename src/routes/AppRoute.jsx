d61220657cfaebaf917143924d1c11feb0ee1561
Seckin Ozturk <franseck102@gmail.com>
2025-01-07 22:08:58 -0500

    contact responsive

--- src/routes/AppRoute.jsx
+++ src/routes/AppRoute.jsx
@@ -3,7 +3,6 @@ import Layout from "../components/Layout"
 import Home from "../components/Home"
 import About from "../components/About"
 import Contact from "../components/Contact"
-
 import Portfolio from "../pages/Portfolio"
 
 
